13684dc3a6d7099296c908f9c54d99de0b0f134c
Yurii <yuri-nevidimkin@yandex.ru>
2023-01-09 23:04:30 +0300

    Котики

---
+++ script/card.js
@@ -0,0 +1,30 @@
+class Card {
+  constructor(data, selectorTemplate) {
+    this._data = data;
+    this._selectorTemplate = selectorTemplate;
+  }
+
+  _getTemplate() {
+    return document
+      .querySelector(this._selectorTemplate)
+      .content.querySelector('.card');
+  }
+  getElement() {
+    this.element = this._getTemplate().cloneNode(true);
+    // console.log(this.element);
+    const cardTitle = this.element.querySelector('.card__name');
+    const cardImage = this.element.querySelector('.card__image');
+    cardTitle.textContent = this._data.name;
+    cardImage.src = this._data.image;
+    // console.log(cardTitle, cardImage);
+
+    return this.element;
+  }
+}
+const card = new Card(cats[0]);
+// console.log(card);
+
+// const template = document.querySelector('#card-template').content.querySelector('.card');
+// console.log(template);
+
+// console.log(card.getElement() === card.getElement());
---
+++ script/cats.js
@@ -0,0 +1,92 @@
+const cats = [
+    {
+        "id": 1234567890,
+        "name": "Чижик",
+        "favorite": true,
+        "rate": 8,
+        "age": 3,
+        "description": "Ласковый, белый и пушистый",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793160_191-p-foto-memnogo-kota-203.jpg"
+    },
+    {
+        "id": 1234567891,
+        "name": "Пыжик",
+        "favorite": false,
+        "rate": 3,
+        "age": 4,
+        "description": "А к нам не подходи, останешься без рук",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793123_185-p-foto-memnogo-kota-197.jpg"
+    },
+    {
+        "id": 1234567892,
+        "name": "Shark",
+        "favorite": true,
+        "rate": 10,
+        "age": 7,
+        "description": "Любит купаться и полотенце душистое",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793117_126-p-foto-memnogo-kota-134.jpg"
+    },
+    {
+        "id": 1234567893,
+        "name": "Персефона",
+        "favorite": false,
+        "rate": 1,
+        "age": 14,
+        "description": "Она видит в тебе только кормушку. Главная в семье.",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793123_147-p-foto-memnogo-kota-157.jpg"
+    },
+    {
+        "id": 1234567894,
+        "name": "Албанец",
+        "favorite": true,
+        "rate": 7,
+        "age": 6,
+        "description": "Хороший, компанейский кот. Всегда рад хозяину",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793120_130-p-foto-memnogo-kota-138.jpg"
+    },
+    {
+        "id": 1234567895,
+        "name": "Мурчик",
+        "favorite": true,
+        "rate": 8,
+        "age": 2,
+        "description": "Игривый, добрый, весёлый",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793170_108-p-foto-memnogo-kota-114.jpg"
+    },
+    {
+        "id": 1234567896,
+        "name": "Пушок",
+        "favorite": false,
+        "rate": 2,
+        "age": 9,
+        "description": "В руки не идёт, гладить не даёт",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793101_101-p-foto-memnogo-kota-107.jpg"
+    },
+    {
+        "id": 1234567897,
+        "name": "Тафик",
+        "favorite": true,
+        "rate": 10,
+        "age": 7,
+        "description": "Станет вам отличным другом. Любит спать в ногах",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793117_11-p-foto-memnogo-kota-12.jpg"
+    },
+    {
+        "id": 1234567898,
+        "name": "Парашют",
+        "favorite": true,
+        "rate": 5,
+        "age": 12,
+        "description": "Любимое дело - прыгать с верхотуры. Мягкий характер.",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793158_122-p-foto-memnogo-kota-129.jpg"
+    },
+    {
+        "id": 1234567899,
+        "name": "Порт",
+        "favorite": true,
+        "rate": 7,
+        "age": 6,
+        "description": "Характер весёлый, игривый. Очень любит рыбий жир.",
+        "image": "https://chudo-prirody.com/uploads/posts/2021-08/1628793041_22-p-foto-memnogo-kota-23.jpg"
+    }
+]
